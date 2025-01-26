7095bb85f49df0257cdc3105aa4c644ea62984f4
BROTKEY <54113864+BROTKEY@users.noreply.github.com>
2025-01-26 11:21:48 +0100

    finish air system

--- game/objects/player/rigid_player.gd
+++ game/objects/player/rigid_player.gd
@@ -8,6 +8,8 @@ extends RigidBody3D
 @export var bubble_time_s: int = 10
 @export var bubble_min: float = 1
 @export var bubble_min_plus_max: float = 1
+@export var bubble_regen_factor: float = 10
+@export var bubble_collider_name_starts_with: String = "rock"
 
 @export var void_level: int = -10
 
@@ -31,13 +33,12 @@ func _process(delta: float) -> void:
 
 func handle_bubble(delta:float) -> void:
 	current_bubble_time -= delta
-	if current_bubble_time < 0:
-		get_tree().reload_current_scene()
-		
 	var scale = current_bubble_time/ bubble_time_s * bubble_min_plus_max + bubble_min
 	$Model/Bubble/BubbleMesh.scale = Vector3(scale, scale,scale)
+	$Collider.scale = Vector3(scale,scale,scale)
 	
-	
+	if current_bubble_time < 0:
+		get_tree().reload_current_scene()
 
 func handle_controls(delta: float):
 	var input := Vector3.ZERO
@@ -86,9 +87,13 @@ func jump():
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	var collision_count = state.get_contact_count()
 	var maybe_on_ground = false
-	for id in range(collision_count):
-		var col_normal = state.get_contact_local_normal(id)
+	for idx in range(collision_count):
+		if state.get_contact_collider_object(idx).name.begins_with(bubble_collider_name_starts_with):
+			if current_bubble_time < bubble_time_s:
+				current_bubble_time += state.step * bubble_regen_factor
+		var col_normal = state.get_contact_local_normal(idx)
 		if col_normal.y > 0.5:
 			maybe_on_ground = true
 	
 	on_ground = maybe_on_ground
+	
